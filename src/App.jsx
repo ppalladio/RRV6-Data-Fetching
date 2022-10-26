cc96b4515bf148d862fddeed2f91ce806c4b8fcd
Yuxuan <nine.slide@gmail.com>
2022-10-26 19:11:05 +0200

    use RouteProvider and useLoaderData (BlogPosts?) use RouteProvider in conjunction with createBrowserRouter&createRoutesElements to rewrite the logic useLoaderData to simplify tbe data fetching processing process

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from 'react-router-dom';
 
 import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage from './pages/BlogPosts';
+import BlogPostsPage, { loader as blogPostLoader } from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
 import PostDetailPage from './pages/PostDetail';
-import RootLayout from './components/RootLayout';
+import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
+const route = createBrowserRouter(
+    createRoutesFromElements(
+        <Route element={<RootLayout />}>
+            <Route index path="/" element={<WelcomePage />} />
+            <Route path="/blog" element={<BlogLayout />}>
+                <Route
+                    index
+                    element={<BlogPostsPage />}
+                    loader={blogPostLoader}
+                />
+                <Route path=":id" element={<PostDetailPage />} />
+            </Route>
+            <Route path="/blog/new" element={<NewPostPage />} />
+        </Route>,
+    ),
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
-  );
+    return <RouterProvider router={route} />;
 }
 
 export default App;
